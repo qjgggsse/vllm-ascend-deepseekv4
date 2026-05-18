dec94893ac238e47f2411f3baa14f34a7cf91677
wangbin <wangbin207@huawei.com>
2026-05-18 10:18:32 +0800

    micro_batch MOE multi-stream coverage function

--- vllm_ascend/ops/fused_moe/prepare_finalize.py
+++ vllm_ascend/ops/fused_moe/prepare_finalize.py
@@ -472,9 +472,10 @@ class PrepareAndFinalizeWithAllGather(PrepareAndFinalize):
 
     def all_gather_input_id_with_dp_group(
         self, input_ids: torch.Tensor) -> torch.Tensor:
-        if self.moe_config.dp_size > 1:
-            forward_context = get_forward_context()
-            moe_local_num_tokens_across_dp = getattr(forward_context, "moe_local_num_tokens_across_dp", None)
+        forward_context = get_forward_context()
+        moe_local_num_tokens_across_dp = getattr(forward_context, "moe_local_num_tokens_across_dp", None)
+        should_gather = moe_local_num_tokens_across_dp is not None or self.moe_config.dp_size > 1
+        if should_gather:
             if moe_local_num_tokens_across_dp is not None:
                 max_tokens_across_dp = int(moe_local_num_tokens_across_dp.max().item())
             else:
